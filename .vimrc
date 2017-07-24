5a5cbc1378641fa2bb353e21dbcd85c0458f69c4
Dmytro Krasovsky <kdi@4dra.net>
2017-07-24 16:35:33 +0200

    simpler setup

--- .vimrc
+++ .vimrc
@@ -1,25 +1,18 @@
 set nocompatible
 filetype off
 
-set rtp+=~/.vim/bundle/Vundle.vim
-call vundle#begin()
-
-Plugin 'VundleVim/Vundle.vim'
-Plugin 'scrooloose/nerdtree.git'
-Plugin 'lumiliet/vim-twig.git'
-
-call vundle#end()
-filetype plugin indent on
-
+set ai
 set et
 set sw=2
 set sts=2
 set smarttab
-
 set number
-
 set mouse=a
+set exrc
+set secure
+set tabstop=4
+set softtabstop=4
+set shiftwidth=4
+set noexpandtab
 
 syntax on
-
-let NERDTreeShowHidden=1
--- apply.sh
+++ apply.sh
@@ -7,17 +7,20 @@ function ensure_file() {
     mkdir -p $HOME/$1;
 
     echo "rm" $HOME/$1/$2;
-    rm $HOME/$1/$2;
+    rm $HOME/$1/$2 || true;
 
     echo "ln -s" $PWD/$1/$2 $HOME/$1/$2;
     ln -s $PWD/$1/$2 $HOME/$1/$2;
+
+	echo "";
 }
 
 ensure_file . .vimrc
 ensure_file . .xinitrc
+ensure_file . .Xresources
 
 ensure_file .config/i3 config
-ensure_file .config/rofi config
-ensure_file .config/termite config
-ensure_file .config/polybar config
-ensure_file .config/gtk-3.0 gtk.css
+ensure_file .config/i3status config
+
+#sbin scripts
+sudo cp scripts/brightnessctl /usr/local/bin/brightnessctl
